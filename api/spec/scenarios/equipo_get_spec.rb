25d14705664ef1629b1343929b8632912b837f32
rhom09 <rhom0909@gmail.com>
2021-07-12 11:11:01 -0300

    Criando um id euipo inexistente

---
+++ api/spec/scenarios/equipo_get_spec.rb
@@ -0,0 +1,44 @@
+describe "GET /equipos/{equipo_id}" do
+  before(:all) do
+    payload = { email: "fenomeno@ig.com.br", password: "090909" }
+    result = Sessions.new.login(payload)
+    @user_id = result.parsed_response["_id"]
+  end
+
+  context "obter unico equipo" do
+    before(:all) do
+      # Dado que eu tenho um novo equipamento
+      @payload = { thumbnail: Helpers::get_thumb("sanfona.jpg"),
+                   name: "Sanfona",
+                   category: "Outros",
+                   price: 499 }
+
+      MongoDB.new.remove_equipo(@payload[:name], @user_id)
+
+      # E eu tenho o id desse equipamento
+      equipo = Equipos.new.create(@payload, @user_id)
+      @equipo_id = equipo.parsed_response["_id"]
+
+      # Quando faço uma requisição get por id
+      @result = Equipos.new.find_by_id(@equipo_id, @user_id)
+    end
+
+    it "deve retornar 200" do
+      expect(@result.code).to eql 200
+    end
+
+    it "deve retornar o nome" do
+      expect(@result.parsed_response).to include("name" => @payload[:name])
+    end
+  end
+
+  context "equipo nao existe" do
+    before(:all) do
+      @result = Equipos.new.find_by_id(MongoDB.new.get_mongo_id, @user_id)
+    end
+
+    it "deve retornar 404" do
+      expect(@result.code).to eql 404
+    end
+  end
+end
