5acb91581ab2981df8b3f5b0ea7b5c32e768fba1
rhom09 <rhom0909@gmail.com>
2021-07-12 11:54:18 -0300

    Automatizando a rota equipos

--- api/spec/scenarios/equipo_get_spec.rb
+++ api/spec/scenarios/equipo_get_spec.rb
@@ -42,3 +42,48 @@ describe "GET /equipos/{equipo_id}" do
     end
   end
 end
+
+describe "GET /equipos" do
+  before(:all) do
+    payload = { email: "flash@ig.com.br", password: "pwd123" }
+    result = Sessions.new.login(payload)
+    @user_id = result.parsed_response["_id"]
+  end
+
+  context "obter uma lista" do
+    before(:all) do
+      payloads = [{
+        thumbnail: Helpers::get_thumb("sanfona.jpg"),
+        name: "Sanfona",
+        category: "Outros",
+        price: 499,
+      }, {
+        thumbnail: Helpers::get_thumb("telecaster.jpg"),
+        name: "Telecaster",
+        category: "Cordas",
+        price: 299,
+      }, {
+        thumbnail: Helpers::get_thumb("violao-nylon.jpg"),
+        name: "Violao Nylon",
+        category: "Cordas",
+        price: 99,
+      }]
+
+      payloads.each do |payload|
+        MongoDB.new.remove_equipo(payload[:name], @user_id)
+        Equipos.new.create(payload, @user_id)
+      end
+
+      # Quando faço uma requisição get para /equipos
+      @result = Equipos.new.list(@user_id)
+    end
+
+    it "deve retornar 200" do
+      expect(@result.code).to eql 200
+    end
+
+    it "deve retornar uma lista de equipos" do
+      expect(@result.parsed_response).not_to be_empty
+    end
+  end
+end
