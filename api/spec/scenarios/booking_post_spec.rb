a441a2877eeb0e7d7cd45741121a355e53384480
rhom09 <rhom0909@gmail.com>
2021-07-12 13:17:31 -0300

    Automatizando a locação de equipos

---
+++ api/spec/scenarios/booking_post_spec.rb
@@ -0,0 +1,34 @@
+describe "POST /equipos/{equipo_id}/bookings" do
+  # Before para pegar o id do usuario, para usar no headers do POST.
+  before(:all) do
+    payload = { email: "ed@gmail.com", password: "pwd123" }
+    result = Sessions.new.login(payload)
+    @ed_id = result.parsed_response["_id"]
+  end
+  context "solicitar locacao" do
+    before(:all) do
+
+      # Dado que "Joe Perry" tem uma "Fender Starto" para locação
+      result = Sessions.new.login({ email: "joe@gmail.com", password: "pwd123" })
+      joe_id = result.parsed_response["_id"]
+
+      fender = {
+        thumbnail: Helpers::get_thumb("fender-sb.jpg"),
+        name: "Fender Strato",
+        category: "Cordas",
+        price: 150,
+      }
+      MongoDB.new.remove_equipo(fender[:name], joe_id)
+
+      result = Equipos.new.create(fender, joe_id)
+      fender_id = result.parsed_response["_id"]
+
+      # Quando solicito a locação da Fender do Joe Perry
+      @result = Equipos.new.booking(fender_id, @ed_id)
+    end
+
+    it "deve retornar 200" do
+      expect(@result.code).to eq 200
+    end
+  end
+end
